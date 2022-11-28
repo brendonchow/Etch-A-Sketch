8b05039e125ca7b406cb538404881c5162ed85ec
Brendon Chow <brendonchow123@gmail.com>
2022-11-28 13:36:23 +1100

    Implement boxes for functionality

--- script.js
+++ script.js
@@ -1,52 +1,38 @@
-const DIMENSION = 500;
 const grid = document.querySelector(".grid");
-
+let boxes;
 
 function createGrid(sideLength) 
 {
     let box, line, i, j;
     grid.replaceChildren();
-    
     for (i = 0; i < sideLength; i++)
     {
         line = document.createElement("div");
-        line.classList.add("row");
-        line.setAttribute("draggable", "false");
-        const boxDimensions = DIMENSION / sideLength;
+        line.classList.add("row")
         for (j = 0; j < sideLength; j++)
         {
             box = document.createElement("div");
             box.classList.add("box");
-            box.style.height = `${boxDimensions}px`;
-            box.style.width = `${boxDimensions}px`;     
-            box.setAttribute("draggable", "false")
-            if (j === 0)
-            {
-                box.style.borderLeftWidth = "1px";
-            }
-            
-            if (i === 0)
-            {
-                box.style.borderTopWidth = "1px";
-            }
-            
+            if (j === 0) box.style.borderLeftWidth = "1px";
+            if (i === 0) box.style.borderTopWidth = "1px";         
             line.appendChild(box);
-            
         }
         grid.appendChild(line);
                    
     }
-    const boxes = document.querySelectorAll(".box");
+    boxes = document.querySelectorAll(".box");
     
     boxes.forEach(element => {
         element.addEventListener("mouseover", (e) => {
-            // Checks that left mouse button is down    
+            // Checks that left mouse button is down
             if (e.buttons === 1) e.target.style.backgroundColor = "black";
         });
         element.addEventListener("dragstart", e => {
-            console.log(e);
             // Stops the drag function
             e.preventDefault();
+        });
+        element.addEventListener("mousedown", (e) => {
+            if (e.buttons === 1) e.target.style.backgroundColor = "black"
         })
     });  
       
@@ -56,6 +42,19 @@ createGrid(10);
 
 
 const slider = document.querySelector(".slider");
-slider.addEventListener("change", () => createGrid(parseInt(slider.value)))
+const sliderValue = document.querySelector(".sliderValue")
+slider.addEventListener("change", e => {
+    sliderValue.textContent = `Grid size: ${e.target.value} x ${e.target.value}`;
+    createGrid(e.target.value);
+});
+
+const backgroundColor = document.querySelector(".backgroundColor");
+// input event listener so color dynamically changes depending on cursor
+backgroundColor.addEventListener("input", e => grid.style.backgroundColor = e.target.value);
+
+const clear = document.querySelector(".clear");
+// clear.addEventListener("click", e => )
+
+const penColor = document.querySelector(".penColor");
 
    
